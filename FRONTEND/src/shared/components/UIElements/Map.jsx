67e0b7c3326e436c99112c1014ba66353bc21977
israbeerwale <israbeerwale529@gmail.com>
2026-09-10 17:15:01 +0530

    switch map from google map to leaflet

--- FRONTEND/src/shared/components/UIElements/Map.jsx
+++ FRONTEND/src/shared/components/UIElements/Map.jsx
@@ -1,21 +1,34 @@
-import { useRef, useEffect } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import "./Map.css";
+
+// Fix for default marker icon not showing in bundlers like Vite
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
 const Map = (props) => {
-  const mapRef = useRef();
-  const { center,zoom } = props;
-  useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-    center: center,
-    zoom: zoom
-  });
-  new window.google.maps.Marker({ position:center, map: map });
-},[center,zoom]);
-return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-    ></div>
+  const { center, zoom } = props;
+  const position = [center.lat, center.lng];
+
+  return (
+    <div className={`map ${props.className}`} style={props.style}>
+      <MapContainer
+        center={position}
+        zoom={zoom}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} />
+      </MapContainer>
+    </div>
   );
 };
 
