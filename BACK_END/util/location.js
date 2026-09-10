2031fd4a111985a00f398e914d17e7a65530cdc1
israbeerwale <israbeerwale529@gmail.com>
2026-09-10 14:32:46 +0530

    switch geocoding to opencage

--- BACK_END/util/location.js
+++ BACK_END/util/location.js
@@ -4,27 +4,23 @@ const HttpError = require("../models/http-error");
 async function getCoordsForAddress(address) {
   //console.log("API KEY:",API_KEY);
   const response = await axios.get(
-    `https://nominatim.openstreetmap.org/search`,
+    `https://api.opencagedata.com/geocoding/v1/json`,
     {
       params:{
         q:address,
-        format:"json",
-        limit:1
+        key: Process.env.OPENCAGE_API_KEY,
+        limit:1,
       },
-      headers:{
-        "User-Agent":"MY_MERN_PROJECT"
-      }
     }
   );
   const data = response.data;
-  console.log(data);
-  if(!data || data.length === 0 ){
+  if(!data || !data.results || data.results.length === 0 ){
     const error = new HttpError('Could not find location for the specified address.',422);
     throw error;
   }
   const coordinates = {
-    lat:parseFloat(data[0].lat),
-    lng:parseFloat(data[0].lon)
+    lat: data.results[0].geometry.lat,
+    lng: data.results[0].geometry.lng
   };
   return coordinates;
 }
